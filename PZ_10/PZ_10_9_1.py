81e917abe69787e56b7186e9e47ffb9b7036776f
Eduard <eduard23rostov@gmail.com>
2022-01-20 23:45:26 +0300

    PZ_10

--- PZ_10/PZ_10_9_1.py
+++ PZ_10/PZ_10_9_1.py
@@ -15,14 +15,14 @@ f3.writelines(l)
 f3.close()
 
 # Дублируем список в новый файл data_4.txt
-f4 = open('data_4.txt', 'w')
+f4 = open('data_4.txt', 'w', encoding="utf-8")
 f4.write('Исходные данные: ')
-f4.write('\n')
+f4.write('')
 f4.writelines(l)
 f4.close()
 
 # разбиваем строку и ее значения преобразуем в числа
-f3 = open('data_3.txt')
+f3 = open('data_3.txt', encoding="utf-8")
 k = f3.read()
 k = k.split()
 for i in range(len(k)):
@@ -31,7 +31,7 @@ f3.close()
 
 # Ищем максимальный элемент и количество отрицательных элементов
 # в файле data_3.txt и записываем в файл data_4.txt
-f3 = open('data_3.txt')
+f3 = open('data_3.txt', encoding="utf-8")
 m = 0
 t = 0
 for i in range(len(k)):
@@ -39,12 +39,19 @@ for i in range(len(k)):
     if int(k[i]) < 0:
         t += 1
 
-f4 = open('data_4.txt', 'a')  # открываем файл для дозаписи
+f4 = open('data_4.txt', 'a', encoding="utf-8")  # открываем файл для дозаписи
 f4.write('\n')
 print('Количество элементов: ', len(k), file=f4)
 f4.close()
 
-f4 = open('data_4.txt', 'a')  # открываем файл для дозаписи
+f4 = open('data_4.txt', 'a', encoding="utf-8")  # открываем файл для дозаписи
 f4.write('')
-print('Максимальный элемент: ', m, file=f4)
+print('Индекс последнего максимального элемента: ', k.index(max(k))+1, file=f4)
 f4.close()
+
+f4 = open('data_4.txt', 'a', encoding="utf-8")  # открываем файл для дозаписи
+f4.write('')
+k[0], k[5], k[1], k[6], k[2], k[7] = k[5], k[0], k[6], k[1], k[7], k[2]
+print('Меняем местами первую и последнюю трети: ', k, file=f4)
+f4.close()
+
